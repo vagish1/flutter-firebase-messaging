3ee8d25fab3147e590d06a92dc5a6367fd8a3154
Vishal Pathak <yoyovishalpathak2004@gmail.com>
2023-04-15 16:38:43 +0530

    Replace context  -> ctx

--- android/src/main/java/io/flutter/plugins/firebase/messaging/FlutterFirebaseMessagingReceiver.java
+++ android/src/main/java/io/flutter/plugins/firebase/messaging/FlutterFirebaseMessagingReceiver.java
@@ -119,7 +119,7 @@ public class FlutterFirebaseMessagingReceiver extends BroadcastReceiver {
           @Override
           public void onResponse(JSONObject response) {
             Log.d("response", response.toString());
-            Toast.makeText(context, "Api Called Successfully", Toast.LENGTH_SHORT).show();
+            Toast.makeText(ctx, "Api Called Successfully", Toast.LENGTH_SHORT).show();
             BookingDetailsModel res = new Gson().fromJson(response.toString(), BookingDetailsModel.class);
             AmbData data = res.getData();
             try {
